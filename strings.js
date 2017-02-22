ddb85193ca8e7314be64a10826af14f3a0769380
qazwsxedcrfvtgb1111 <tttdima60@gmail.com>
2017-02-23 01:32:30 +0200

    sprintf fix

--- strings.js
+++ strings.js
@@ -2,7 +2,7 @@
  * Created by Dima on 2/23/2017.
  */
 export const strings = {
-    users: 'slushaem pocanov: %f',
+    users: 'slushaem pocanov: %s',
     not_found_fm: 'last fm ne znaet kto ti poprobuy po drugomu /reg_fm fm_username',
     not_found_db: 'tebya net v bd delay /reg_fm fm_username',
     help:
